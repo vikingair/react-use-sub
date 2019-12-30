a6adc91ba9a1bcb14b982f0c4e84c93363687a72
Viktor Luft <viktor.luft@freiheit.com>
2019-12-30 09:08:31 +0100

    Update all deps

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,4 @@
 import babel from 'rollup-plugin-babel';
-import resolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
 import flowEntry from 'rollup-plugin-flow-entry';
 
 export default {
@@ -8,18 +6,19 @@ export default {
     plugins: [
         babel({
             exclude: 'node_modules/**',
-            presets: [['@babel/preset-env', { modules: false, targets: { node: "8" } }]]
+            presets: [['@babel/preset-env', { modules: false, targets: { node: '8' } }]],
         }),
-        resolve(),
-        commonjs(),
         flowEntry(),
     ],
     external: ['react', 'react-dom'],
-    output: [{
-        dir: 'dist/cjs',
-        format: 'cjs'
-    }, {
-        dir: 'dist/esm',
-        format: 'esm'
-    }]
+    output: [
+        {
+            dir: 'dist/cjs',
+            format: 'cjs',
+        },
+        {
+            dir: 'dist/esm',
+            format: 'esm',
+        },
+    ],
 };
