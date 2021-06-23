680416b496e1b3fed4248c54873d666bc02ef282
Viktor Luft <viktor.luft@freiheit.com>
2021-06-23 15:12:07 +0200

    Update all deps

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 export default {
-  collectCoverageFrom: ['src/**/*.{ts,tsx}'],
-  testMatch: ['<rootDir>/test/**/*.{ts,tsx}'],
-  coverageThreshold: { global: { statements: 100, branches: 100, functions: 100, lines: 100 } },
-  coverageDirectory: "<rootDir>/build/coverage"
+    collectCoverageFrom: ['src/**/*.{ts,tsx}'],
+    testMatch: ['<rootDir>/test/**/*.{ts,tsx}'],
+    coverageThreshold: { global: { statements: 100, branches: 100, functions: 100, lines: 100 } },
+    coverageDirectory: "<rootDir>/build/coverage",
+    testEnvironment: 'jsdom',
 };
