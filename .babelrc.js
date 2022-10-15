58edd8f2ab13f83a4b2e068f2701701fd9eb9750
Viktor Luft <viktor.luft@freiheit.com>
2022-10-15 17:23:39 +0200

    Fix SSR

--- .babelrc.js
+++ .babelrc.js
@@ -1,6 +1,6 @@
 const { NODE_ENV, BABEL_ENV } = process.env
 const cjs = NODE_ENV === 'test' || BABEL_ENV === 'commonjs'
-const envConfig = cjs ? { targets: { node: 12 }} : {
+const envConfig = cjs ? { targets: { node: 14 }} : {
   targets: { esmodules: true },
   bugfixes: true,
   modules: false,
