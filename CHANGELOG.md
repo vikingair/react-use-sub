cc85cb3ea9a7873d9d6c4726b1ae969fd1399a51
Viktor Luft <viktor.luft@freiheit.com>
2024-05-30 08:08:02 +0200

    Update all deps (#10)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,87 +1,129 @@
 # Changelog
+
 All notable changes to this project will be documented in this file.
 
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [4.0.0] - 2024-05-30
+
+### Changed
+
+- Dropped support for Node < 18
+- Using `act` from `@testing-library/react` in test util to avoid warnings being printed
+
 ## [3.0.1] - 2022-10-15
+
 ### Fixed
+
 - SSR was not working properly, because of missing mandatory `getServerSnapshot` implementation.
 
 ### Changed
-- Dropped Node12 support. At least Node14 required now. 
 
+- Dropped Node12 support. At least Node14 required now.
 
 ## [3.0.0] - 2022-06-07
+
 ### Added
+
 - [Migration Guide Next](https://github.com/fdc-viktor-luft/react-use-sub/blob/master/MIGRATIONGUIDE.md#300)
 
 ### Changed
+
 - Not compatible anymore to earlier versions of React than 18, because
   it makes use of the new hook `useSyncExternalStore` and relies on automatic batching.
 
 ## [2.2.2] - 2022-02-15
+
 ### Fixed
+
 - Generating type interface instead of shipping real types to prevent TS errors for consumers.
 
 ## [2.2.1] - 2021-11-01
+
 ### Fixed
+
 - Include React 17 as valid peer dependency.
 
 ## [2.2.0] - 2021-07-06
+
 ### Changed
+
 - Optional state does not need to be initialized with explicit `undefined` value anymore.
   The store updates allow now also to set data that does not belong into the store. Using
   strict TS checks, should avoid this to happen, so it's not enforced by the given code
   anymore.
 
 ## [2.1.1] - 2021-06-30
+
 ### Fix
+
 - NextJS stopped working because setting `"type": "module"` in own `package.json` was overriding
   the default of the package importer. Hence, the package could only be used as ESM.
 
 ## [2.1.0] - 2021-05-02
+
 ### Added
+
 - `Store.listen` allows adding (and removing) listeners on the store outside react components.
 
 ## [2.0.0] - 2021-03-21
+
 ### Added
+
 - [Migration Guide v2](https://github.com/fdc-viktor-luft/react-use-sub/blob/master/MIGRATIONGUIDE.md#200)
 
 ### Changed
+
 - Allowing `undefined` types on top-level thanks to
   [hint](https://github.com/microsoft/TypeScript/issues/13195#issuecomment-802213410)
   of [@InExtremaRes](https://github.com/InExtremaRes).
 
 ## [1.2.0] - 2021-03-17
+
 ### Added
+
 - Test-Util `react-use-sub/test-util`. [More details](https://github.com/fdc-viktor-luft/react-use-sub#testing).
 
 ## [1.1.1] - 2021-03-14
+
 ### Changed
+
 - Node version >= 12 requirement. Possibly still works with older Node versions.
 
 ## [1.1.0] - 2020-09-03
+
 ### Added
+
 - Conditional store updates.
 
 ## [1.0.4] - 2020-09-03
+
 ### Added
+
 - Improve IDE auto-import by moving index file into root directory.
 
 ## [1.0.3] - 2020-08-28
+
 ### Added
+
 - Fix rarely omitted re-rendering
 
 ## [1.0.2] - 2020-08-17
+
 ### Added
+
 - Fix error on update with external deps
 
 ## [1.0.1] - 2020-04-08
+
 ### Added
+
 - Export some utility types
 - Lower peer deps requirements
 
 ## [1.0.0] - 2020-02-06
+
 ### Added
+
 - Publish first stable version
--- test/test-util.test.tsx
+++ test/test-util.test.tsx
@@ -1,132 +1,151 @@
-import { describe, it, expect, vi } from 'vitest';
-import React from 'react';
-import { render } from '@testing-library/react';
-import { createStore, _config } from '../src';
-import { act } from 'react-dom/test-utils';
+import { describe, expect, it, vi } from "vitest";
+import React from "react";
+import { act, render } from "@testing-library/react";
+import { _config, createStore } from "../src";
 
 _config.batch = act as any;
 _config.dispatch = (fn): any => fn();
+_config.onError = (errMsg) => {
+  throw new Error(errMsg);
+};
 
-describe('test-util', () => {
-    it('allows to update the react state without the need of jest.runAllTimers', () => {
-        const [useSub, Store] = createStore({ foo: true });
-        let currentReceived: any = null;
-        let renderCount = 0;
-        const Dummy = ({ bool }: { bool: boolean }) => {
-            ++renderCount;
-            currentReceived = useSub(({ foo }) => foo === bool);
-            return null;
-        };
-        const { rerender } = render(<Dummy bool={true} />);
+describe("test-util", () => {
+  it("allows to update the react state without the need of jest.runAllTimers", () => {
+    const [useSub, Store] = createStore({ foo: true });
+    let currentReceived: any = null;
+    let renderCount = 0;
+    const Dummy = ({ bool }: { bool: boolean }) => {
+      ++renderCount;
+      currentReceived = useSub(({ foo }) => foo === bool);
+      return null;
+    };
+    const { rerender } = render(<Dummy bool={true} />);
 
-        // initial render
-        expect(renderCount).toBe(1);
-        expect(currentReceived).toBe(true);
+    // initial render
+    expect(renderCount).toBe(1);
+    expect(currentReceived).toBe(true);
 
-        // rerender the component with different bool
-        rerender(<Dummy bool={false} />);
+    // rerender the component with different bool
+    rerender(<Dummy bool={false} />);
 
-        // the dep array changed and the value needs to be recomputed
-        expect(renderCount).toBe(2);
-        expect(currentReceived).toBe(false);
+    // the dep array changed and the value needs to be recomputed
+    expect(renderCount).toBe(2);
+    expect(currentReceived).toBe(false);
 
-        // update the store value so that we receive the original value which was also computed last time
-        // but only by dependency change
-        Store.set({ foo: false });
+    // update the store value so that we receive the original value which was also computed last time
+    // but only by dependency change
+    Store.set({ foo: false });
 
-        expect(renderCount).toBe(3);
-        expect(currentReceived).toBe(true);
+    expect(renderCount).toBe(3);
+    expect(currentReceived).toBe(true);
 
-        // does re-render twice instead of not-updating when test-util is activated
-        Store.set({ foo: true });
-        Store.set({ foo: false });
+    // does re-render twice instead of not-updating when test-util is activated
+    Store.set({ foo: true });
+    Store.set({ foo: false });
 
-        expect(renderCount).toBe(5);
-        expect(currentReceived).toBe(true);
+    expect(renderCount).toBe(5);
+    expect(currentReceived).toBe(true);
+  });
+
+  it("allows to trigger listeners without the need of jest.runAllTimers", () => {
+    const spy = vi.fn();
+    const [, Store] = createStore({ foo: "bar", num: 42 });
+
+    // when
+    const removeListener = Store.listen(
+      ({ foo, num }) => ({ odd: num % 2 === 1, fooLength: foo.length }),
+      (next, prev) => {
+        // don't make it shorter by putting the spy as listener, because we are also testing the TS integration
+        spy({
+          odd: next.odd as boolean,
+          fooLength: next.fooLength as number,
+          prevOdd: prev.odd as boolean,
+          prevFooLength: prev.fooLength as number,
+        });
+
+        // return value will be ignored and can be of any type
+        return "foo";
+      },
+    );
+
+    // initially
+    expect(spy).not.toHaveBeenCalled();
+
+    // when - updating without changing the length of "foo"
+    Store.set({ foo: "wha" });
+
+    // then
+    expect(spy).not.toHaveBeenCalled();
+
+    // when - updating length of "foo"
+    Store.set({ foo: "what" });
+
+    expect(spy).toHaveBeenCalledTimes(1);
+    expect(spy).toHaveBeenCalledWith({
+      odd: false,
+      fooLength: 4,
+      prevOdd: false,
+      prevFooLength: 3,
     });
 
-    it('allows to trigger listeners without the need of jest.runAllTimers', () => {
-        const spy = vi.fn();
-        const [, Store] = createStore({ foo: 'bar', num: 42 });
+    spy.mockReset();
+    Store.set({ foo: "yo" });
+    Store.set({ num: 13 });
 
-        // when
-        const removeListener = Store.listen(
-            ({ foo, num }) => ({ odd: num % 2 === 1, fooLength: foo.length }),
-            (next, prev) => {
-                // don't make it shorter by putting the spy as listener, because we are also testing the TS integration
-                spy({
-                    odd: next.odd as boolean,
-                    fooLength: next.fooLength as number,
-                    prevOdd: prev.odd as boolean,
-                    prevFooLength: prev.fooLength as number,
-                });
-
-                // return value will be ignored and can be of any type
-                return 'foo';
-            }
-        );
-
-        // initially
-        expect(spy).not.toHaveBeenCalled();
-
-        // when - updating without changing the length of "foo"
-        Store.set({ foo: 'wha' });
-
-        // then
-        expect(spy).not.toHaveBeenCalled();
-
-        // when - updating length of "foo"
-        Store.set({ foo: 'what' });
-
-        expect(spy).toHaveBeenCalledTimes(1);
-        expect(spy).toHaveBeenCalledWith({ odd: false, fooLength: 4, prevOdd: false, prevFooLength: 3 });
-
-        spy.mockReset();
-        Store.set({ foo: 'yo' });
-        Store.set({ num: 13 });
-
-        // be aware the test utils don't process all requests batched but sequential, hence all updates are received
-        // individually for every call of "Store.set"
-        expect(spy).toHaveBeenCalledTimes(2);
-        expect(spy).toHaveBeenCalledWith({ odd: false, fooLength: 2, prevOdd: false, prevFooLength: 4 });
-        expect(spy).toHaveBeenCalledWith({ odd: true, fooLength: 2, prevOdd: false, prevFooLength: 2 });
-
-        spy.mockReset();
-        removeListener();
-
-        Store.set({ foo: 'update' });
-        expect(spy).not.toHaveBeenCalled();
+    // be aware the test utils don't process all requests batched but sequential, hence all updates are received
+    // individually for every call of "Store.set"
+    expect(spy).toHaveBeenCalledTimes(2);
+    expect(spy).toHaveBeenCalledWith({
+      odd: false,
+      fooLength: 2,
+      prevOdd: false,
+      prevFooLength: 4,
+    });
+    expect(spy).toHaveBeenCalledWith({
+      odd: true,
+      fooLength: 2,
+      prevOdd: false,
+      prevFooLength: 2,
     });
 
-    it('works with shallow comparable array subscriptions', () => {
-        const [useSub, Store] = createStore<{ aMap: Record<string, string> }>({ aMap: { '42': 'foo', '44': 'bar' } });
-        let currentReceived: any = null;
-        let renderCount = 0;
-        const Dummy = () => {
-            ++renderCount;
-            currentReceived = useSub(({ aMap }) => Object.keys(aMap));
-            return null;
-        };
+    spy.mockReset();
+    removeListener();
 
-        // initial render
-        render(<Dummy />);
-        expect(renderCount).toBe(1);
-        expect(currentReceived).toEqual(['42', '44']);
+    Store.set({ foo: "update" });
+    expect(spy).not.toHaveBeenCalled();
+  });
 
-        // change keys
-        Store.set({ aMap: { '103': 'foo', '404': 'bar' } });
-
-        // then
-        expect(renderCount).toBe(2);
-        expect(currentReceived).toEqual(['103', '404']);
+  it("works with shallow comparable array subscriptions", () => {
+    const [useSub, Store] = createStore<{ aMap: Record<string, string> }>({
+      aMap: { "42": "foo", "44": "bar" },
     });
+    let currentReceived: any = null;
+    let renderCount = 0;
+    const Dummy = () => {
+      ++renderCount;
+      currentReceived = useSub(({ aMap }) => Object.keys(aMap));
+      return null;
+    };
 
-    // TODO: Make it easier for user to handle errors caused by returned objects not being shallow comparable
-    //       on sub-sequent runs.
-    //       Current error message would be:
-    //           Maximum update depth exceeded. This can happen when a component repeatedly calls setState inside
-    //           componentWillUpdate or componentDidUpdate. React limits the number of nested updates to prevent
-    //           infinite loops.
-    //       Could cause confusion for users of the lib even though it is pretty valuable information and can be easily
-    //       avoided by combining "useSub" with "useMemo".
+    // initial render
+    render(<Dummy />);
+    expect(renderCount).toBe(1);
+    expect(currentReceived).toEqual(["42", "44"]);
+
+    // change keys
+    Store.set({ aMap: { "103": "foo", "404": "bar" } });
+
+    // then
+    expect(renderCount).toBe(2);
+    expect(currentReceived).toEqual(["103", "404"]);
+  });
+
+  // TODO: Make it easier for user to handle errors caused by returned objects not being shallow comparable
+  //       on sub-sequent runs.
+  //       Current error message would be:
+  //           Maximum update depth exceeded. This can happen when a component repeatedly calls setState inside
+  //           componentWillUpdate or componentDidUpdate. React limits the number of nested updates to prevent
+  //           infinite loops.
+  //       Could cause confusion for users of the lib even though it is pretty valuable information and can be easily
+  //       avoided by combining "useSub" with "useMemo".
 });
